bcbe1e4d852f201e3a675f9414e8bee58ebd3451
J-KOEL <jaysonkoel05@gmail.com>
2025-07-17 11:50:46 -0500

    Create data_loader.py

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def load_mapping(file_path):
+    df = pd.read_csv(file_path)
+    df.columns = df.columns.str.strip()
+    df = df.dropna(how='all', axis=1)
+    df = df.dropna(how='all')
+    return dict(zip(df['Label'].str.strip(), df['Code'].astype(str).str.strip()))
